ab746fec6928752f4c57d4784e9863004f9a107f
Jack Kennedy <S00235062@atu.ie>
2023-01-27 10:40:34 +0000

    Question 2 Part b

--- Question 2/Program.cs
+++ Question 2/Program.cs
@@ -13,14 +13,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Part a");
-            CountOddNumbers();
+            int oddnumbers = CountOddNumbers();
+            Console.WriteLine($"You entered in {oddnumbers} odd numbers");
+
+            Console.WriteLine("\nPart b");
+            AverageMinMax();
         }
 
-        static void CountOddNumbers() // part a
+        static int CountOddNumbers() // part a
         {
             //declare variables
             int[] numbers = new int[5];
-            int i = 0, oddnumbers = 0;
+            int i = 0, oddnumbers = 1;
 
             for (i = 0; i < numbers.Length; i++)
             {
@@ -36,7 +40,46 @@
                 }
             }
 
-            Console.WriteLine($"You entered in {oddnumbers} odd numbers");
+            return oddnumbers;
+        }
+        static void AverageMinMax() // part b
+        {
+            // declare variables
+            int[] numbers = new int[5];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{"Enter a number", -20} : ");
+                numbers[i] = int.Parse(Console.ReadLine());
+                
+            }
+
+            //find smallest number
+            int smallestNumber=numbers[0];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (smallestNumber > numbers[i])
+                    smallestNumber = numbers[i];
+            }
+            Console.WriteLine($"The smallest number you entered was {smallestNumber}");
+
+            //find largest number
+            int biggestNumber = numbers[0];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (biggestNumber < numbers[i])
+                    biggestNumber = numbers[i];
+            }
+            Console.WriteLine($"The largest number you entered was {biggestNumber}");
+
+            //find average
+            int average = 0, total = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                total = total + numbers[i];
+            }
+            average = total / numbers.Length;
+            Console.WriteLine($"The average of the numbers you entered was {average}");
         }
     }
 }
