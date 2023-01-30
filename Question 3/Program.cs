52115dd1a160dd459e927451d9acca5dabd6b62b
Jack Kennedy <S00235062@atu.ie>
2023-01-30 09:32:04 +0000

    Question 3 Parts b and c

--- Question 3/Program.cs
+++ Question 3/Program.cs
@@ -20,22 +20,71 @@
         {
             Console.WriteLine("Part a");
             PrintTriangle();
+
+            Console.WriteLine("\nPart b");
+            PrintTriangleUpsideDown();
+
+            Console.WriteLine("\nPart c");
+            //PrintTriangleGeneral();
+
+            Console.WriteLine("\nPart d");
+            //PrintTriangle();
         }
-        static void PrintTriangle()
+        static void PrintTriangle() // part a
         {
             string hashtag = "#";
             int i = 0;
             while (5 >= i)
             {
                 int counter = 0;
-                while (counter > i)
+                while (counter < i)
                 {
-                    Console.WriteLine(hashtag);
+                    Console.Write(hashtag);
                     counter++;
                 }
+                Console.Write($"\n");
                 i++;
             }
-            
+
+        }
+
+        static void PrintTriangleUpsideDown() // part c
+        {
+            string hashtag = "#";
+            int i = 5;
+            while (0 <= i)
+            {
+                int counter = 0;
+                while (counter < i)
+                {
+                    Console.Write(hashtag);
+                    counter++;
+                }
+                Console.Write($"\n");
+                i--;
+            }
+        }
+
+        static void PrintTriangleGeneral()
+        {
+            string hashtag = "#";
+            int x = 0;
+
+            //user input
+            Console.Write($"{"Enter a number of rows for your triangle", -40} : ");
+            int i = int.Parse(Console.ReadLine());
+
+            while (i >= x)
+            {
+                int counter = 0;
+                while (counter < x)
+                {
+                    Console.Write(hashtag);
+                    counter++;
+                }
+                Console.Write($"\n");
+                x++;
+            }
         }
     }
 }
